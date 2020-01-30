c7243281c331841d1e91aaa5bee53a4f71dba117
Oleh18Halat05 <46803779+Oleh18Halat05@users.noreply.github.com>
2020-01-30 02:21:21 -0800

    Updates

---
+++ precache-manifest.e79caff25b96f65c1312524d472d0eed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d29c33bad1563c942da907c74e646ad",
+    "url": "/project/index.html"
+  },
+  {
+    "revision": "17a0f4909219f95e6eb4",
+    "url": "/project/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e82ddedc211108d0cb70",
+    "url": "/project/static/js/2.e8b47e5f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/project/static/js/2.e8b47e5f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "17a0f4909219f95e6eb4",
+    "url": "/project/static/js/main.41ea88b2.chunk.js"
+  },
+  {
+    "revision": "e1f18bd0a5bac32c8440",
+    "url": "/project/static/js/runtime-main.519632d1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project/static/media/logo.5d5d9eef.svg"
+  }
+]);
